674f1dbf1f1885d2d8649adf41664642fafe6f64
Kiem Nguyen <kiemnguyen189@gmail.com>
2021-01-19 03:45:55 +0000

    Started building openings.

--- COVID-19 Simulation/Assets/Scripts/BuildingManager.cs
+++ COVID-19 Simulation/Assets/Scripts/BuildingManager.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class BuildingManager : MonoBehaviour
 {
     
-    
+    public Transform opening1;
+    public Transform opening2;
+    public Transform opening3;
+    public Transform opening4;
+
+    public int radius;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(opening1.position);
+        Debug.Log(opening2.position);
+        Debug.Log(opening3.position);
+        Debug.Log(opening4.position);
     }
 
     // Update is called once per frame
